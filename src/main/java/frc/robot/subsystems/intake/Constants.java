172e3a835b8c94be72db1dbabab86cf1d35595bd
Yehuda Rothstein <129756006+YehudaRothstein@users.noreply.github.com>
2025-06-09 15:43:57 +0300

    Intake Subsystem (#2)
    * add intake subsystem and state management
    
    * Added states for L1 scoring and getting coral back from arm.
    
    * Added states for L1 scoring and getting coral back from arm.
    
    * Test Commit
    
    ---------
    
    Co-authored-by: ExcaliburFRC <excaliburfrc@gmail.com>

---
+++ src/main/java/frc/robot/subsystems/intake/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.intake;
+
+public class Constants {
+    public static final int ARM_MOTOR_ID = 0;
+    public static final int ROLLERS_MOTOR_ID = 0;
+    public static final int CENTERLIZER_MOTOR_ID = 0;
+    public static final double ARM_VELOCITY_MIN = 0;
+    public static final double ARM_VELOCITY_MAX = 0;
+    public static final int ENCODER_ID = 0;
+    public static final int TOLERANCE = 0;
+
+    public static final int SENSOR_CHANNEL = 0;
+}
---
+++ src/main/java/frc/robot/subsystems/intake/IntakeState.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.intake;
+
+public enum IntakeState {
+    FLOOR_INTAKE(0,0,0),
+    STOW(0,0,0),
+    PASS_TO_ARM(0,0,0),
+    L1_SCORE(0, 0, 0),
+    GET_CORAL_BACK_FROM_ARM(0, 0, 0);
+    final double intakeAngle;
+    final double rollerVoltage;
+    final double centraliserVoltage;
+    private IntakeState(int intakeAngle, int rollerVoltage, int centraliserVoltage){
+        this.intakeAngle = intakeAngle;
+        this.rollerVoltage = rollerVoltage;
+        this.centraliserVoltage = centraliserVoltage;
+    }
+}
