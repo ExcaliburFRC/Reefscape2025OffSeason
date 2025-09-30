e1fbddc9e687938b8f89ac1cb21cee7c137f2f61
Yehuda Rothstein <129756006+YehudaRothstein@users.noreply.github.com>
2025-09-30 16:12:28 +0300

    updated measurements

--- src/main/java/frc/robot/subsystems/intake/IntakeState.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeState.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems.intake;
 
 public enum IntakeState {
-    FLOOR_INTAKE(3.08,-0.5,0),
+    FLOOR_INTAKE(3.08,-0.8,0),
     CENTERLIZE(2,0,0.4),
-    DEFAULT(0.8635,0,0),
+    DEFAULT(0.8635,-0.1,0.2),
     PRE_HANDOFF(0.8635,0,0),
     HANDOFF(0.8635,0.8,0),
     L1_SCORE_PRE(1.352, 0, 0.1),
