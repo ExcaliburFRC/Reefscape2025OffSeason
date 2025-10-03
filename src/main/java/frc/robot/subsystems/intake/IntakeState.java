9f3f01791b4bfc3677a9d7d0db6b5cf8d2a28b43
Yehuda Rothstein <129756006+YehudaRothstein@users.noreply.github.com>
2025-10-03 14:35:10 +0300

    intake states

--- src/main/java/frc/robot/subsystems/intake/IntakeState.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeState.java
@@ -4,15 +4,18 @@ public enum IntakeState {
     FLOOR_INTAKE(3.08,-0.25,0),
     CENTERLIZE(0.8635,-0.25,3),
     DEFAULT(0.8635,-0.1,0.2),
-    PRE_HANDOFF(0.8635,0,0),
     HANDOFF(0.8635,0.8,0),
+
     L1_SCORE_PRE(1.352, 0, 0.1),
     L1_SCORE(1.352, 0.1,0),
+
     REVERSE_HANDOFF(0.8635, -0.3, 0.1),
     EJECT_CORAL(2, 0.5, 0);
+
     final double intakeAngle;
     final double rollerVoltage;
     final double centraliserVoltage;
+
     IntakeState(double intakeAngle, double rollerVoltage, double centraliserVoltage){
         this.intakeAngle = intakeAngle;
         this.rollerVoltage = rollerVoltage;
